eabeaf01739a71fe24bc062044688ea42fc1b94e
darthchudi <chukwudioranu@ymail.com>
2019-01-25 01:08:51 +0100

    fix: update env config

--- src/common/config/env.ts
+++ src/common/config/env.ts
@@ -4,13 +4,7 @@ dotenv.config();
 
 export default {
   env: process.env.NODE_ENV || 'development',
-  mailgun_api_key: process.env.MAILGUN_API_KEY,
-  mailgun_domain: process.env.MAILGUN_DOMAIN,
-  mailgun_email: process.env.MAILGUN_EMAIL,
-  mongodb_uri:
-    process.env.MONGODB_URL || 'mongodb://localhost:27017/imperium_dev',
+  mongodb_url: process.env.MONGODB_URL,
   port: Number(process.env.PORT) || 3080,
   redis_url: process.env.REDIS_URL || 'redis://localhost:6379',
-  salt_rounds: process.env.SALT_ROUNDS || 10,
-  jwt_secret: process.env.JWT_SECRET,
 };
