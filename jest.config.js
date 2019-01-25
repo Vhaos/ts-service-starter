4ae1123d642cdaacbb6b0c1e7738d7230db20e9a
darthchudi <chukwudioranu@ymail.com>
2019-01-25 01:08:32 +0100

    feat: add jest and ts-jest config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@app/common(.*)$': '<rootDir>/src/common$1',
+    '^@app/data(.*)$': '<rootDir>/src/data$1',
+    '^@app/server(.*)$': '<rootDir>/src/server$1',
+    '^@app(.*)$': '<rootDir>/src$1',
+  },
+};
